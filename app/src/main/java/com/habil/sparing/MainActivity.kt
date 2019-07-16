24e6efa5fc1dde0bd6aa0d1d30b416e5a2a95cdd
habil mahendri <habilmahendri@gmail.com>
2019-07-16 20:25:37 +0700

    fixing code bottom nav

--- app/src/main/java/com/habil/sparing/MainActivity.kt
+++ app/src/main/java/com/habil/sparing/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.habil.sparing
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
@@ -13,75 +14,53 @@ import com.habil.sparing.feature.notification.NotificationFragment
 
 
 class MainActivity : AppCompatActivity() {
+    val home: Fragment = HomeFragment()
+    val explore: Fragment = ExploreFragment()
+    val lobby: Fragment = LobbyFragment()
+    val notif: Fragment = NotificationFragment()
+
+    val fm = supportFragmentManager
+    var active = home
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    loadHomeFragment(savedInstanceState)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_explore -> {
-                    loadExploreFragment(savedInstanceState)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_lobby -> {
-                    loadLobbyFragment(savedInstanceState)
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.navigation_notifications -> {
-                    loadNotifFragment(savedInstanceState)
-                    return@OnNavigationItemSelectedListener true
-                }
-            }
-            false
-        }
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
 
-    private fun loadHomeFragment(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_container, HomeFragment(), HomeFragment::class.java.simpleName)
-                .commit()
+        fm.beginTransaction().add(R.id.main_container, notif, "4").hide(notif).commit()
+        fm.beginTransaction().add(R.id.main_container, lobby, "3").hide(lobby).commit()
+        fm.beginTransaction().add(R.id.main_container, explore, "2").hide(explore).commit()
+        fm.beginTransaction().add(R.id.main_container, home, "1").commit()
+
+    }
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                fm.beginTransaction().hide(active).show(home).commit()
+                active = home
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_explore -> {
+                fm.beginTransaction().hide(active).show(explore).commit()
+                active = explore
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_lobby -> {
+                fm.beginTransaction().hide(active).show(lobby).commit()
+                active = lobby
+                return@OnNavigationItemSelectedListener true
+            }R.id.navigation_notifications -> {
+                fm.beginTransaction().hide(active).show(notif).commit()
+                active = notif
+
+                return@OnNavigationItemSelectedListener true
+            }
         }
+        false
     }
-
-    private fun loadExploreFragment(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_container, ExploreFragment(), ExploreFragment::class.java.simpleName)
-                .commit()
-        }
-    }
-
-    private fun loadLobbyFragment(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.main_container, LobbyFragment(), LobbyFragment::class.java.simpleName)
-                    .commit()
-        }
-    }
-
-    private fun loadNotifFragment(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_container, NotificationFragment(), NotificationFragment::class.java.simpleName)
-                .commit()
-        }
-    }
-
-
-
 
 
 }

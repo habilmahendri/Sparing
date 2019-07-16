5fdff133419c303e2e24063bb04bc0af83ca2872
habil mahendri <habilmahendri@gmail.com>
2019-07-16 18:54:59 +0700

    introo

--- app/src/main/java/com/habil/sparing/feature/splashscreen_intro/IntroFragment.kt
+++ app/src/main/java/com/habil/sparing/feature/splashscreen_intro/IntroFragment.kt
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.habil.sparing.R
+import com.habil.sparing.feature.login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_intro.*
+import org.jetbrains.anko.intentFor
 
 
 class IntroFragment : Fragment() {
@@ -20,5 +22,13 @@ class IntroFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_intro, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_lanjutkan.setOnClickListener {
+            startActivity(context!!.intentFor<LoginActivity>())
+            activity!!.finishAffinity()
+        }
+    }
 
 }

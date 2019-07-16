ee40a1400f68e7cebc93d842953ee3df1a9aa04a
habil mahendri <habilmahendri@gmail.com>
2019-07-16 18:17:15 +0700

    add login

--- app/src/main/java/com/habil/sparing/feature/SplashScreen.kt
+++ app/src/main/java/com/habil/sparing/feature/SplashScreen.kt
@@ -7,26 +7,34 @@ import android.os.Handler
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import com.habil.adoption.data.PreferencesHelper
 import com.habil.sparing.MainActivity
 import com.habil.sparing.R
 import com.habil.sparing.feature.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var preferencesHelper: PreferencesHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //supportActionBar!!.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         setContentView(R.layout.activity_splash_screen)
+        preferencesHelper = PreferencesHelper()
 
 
-
-        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.app_splash)
+        val animation:Animation = AnimationUtils.loadAnimation(this, R.anim.app_splash)
         splash.startAnimation(animation)
 
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+
+            if (preferencesHelper.getLoggedStatus(this)) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+
+            }
             finish()
         }, 1500)
     }

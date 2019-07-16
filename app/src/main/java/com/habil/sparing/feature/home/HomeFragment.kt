f5087b20e8b7a772df7acf6288782d5bfa71ce04
habil mahendri <habilmahendri@gmail.com>
2019-07-16 21:01:18 +0700

    add category

--- app/src/main/java/com/habil/sparing/feature/home/HomeFragment.kt
+++ app/src/main/java/com/habil/sparing/feature/home/HomeFragment.kt
@@ -3,22 +3,18 @@ package com.habil.sparing.feature.home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.text.HtmlCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.habil.adoption.data.PreferencesHelper
 
 import com.habil.sparing.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class HomeFragment : Fragment() {
+    private lateinit var preferencesHelper: PreferencesHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +22,18 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        preferencesHelper = PreferencesHelper()
+
+        val fullName:String? = preferencesHelper.getFullName(context!!)
+        tv_greeting.text =  HtmlCompat.fromHtml(
+            "Hello <b>$fullName<br>Mau main apa hari ini ?</b> ",
+            HtmlCompat.FROM_HTML_MODE_LEGACY)
+
     }
 
 

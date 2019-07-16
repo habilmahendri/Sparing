72bd149f69f6f605ebbfe44da366a5553ecb1e89
habil mahendri <habilmahendri@gmail.com>
2019-07-16 16:37:29 +0700

    add login activity

--- app/src/main/java/com/habil/sparing/feature/SplashScreen.kt
+++ app/src/main/java/com/habil/sparing/feature/SplashScreen.kt
@@ -1,13 +1,30 @@
 package com.habil.sparing.feature
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.habil.sparing.MainActivity
 import com.habil.sparing.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_splash_screen)
+
+
+
+        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.app_splash)
+        splash.startAnimation(animation)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
---
+++ app/src/main/java/com/habil/sparing/feature/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.habil.sparing.feature.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.habil.sparing.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
